fb825ac24b36e112e573c8c6f6b4ac052b85c1f9
thelostsoul <1095533751@qq.com>
2018-09-13 15:13:35 +0800

    注释

--- src/main/java/xyz/thelostsoul/base/split/inter/ISplitFieldParser.java
+++ src/main/java/xyz/thelostsoul/base/split/inter/ISplitFieldParser.java
@@ -3,6 +3,17 @@ package xyz.thelostsoul.base.split.inter;
 import java.util.List;
 
 public interface ISplitFieldParser {
+    /**
+     * 处理分表字段，得到分表索引
+     * @param var1 分表字段值
+     * @return 分表索引
+     * @throws Exception 取值错误
+     */
     String convert(Object var1) throws Exception;
+
+    /**
+     * 当查询条件不存在分表索引的时候，返回默认的索引列表
+     * @return 索引列表
+     */
     List<String> all();
 }
