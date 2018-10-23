af47b48eb3897c90488168f4076a7de19eb037a6
thelostsoul <1095533751@qq.com>
2018-10-23 19:30:17 +0800

    JSON格式化工具

---
+++ src/main/java/xyz/thelostsoul/utils/JsonFormatter.java
@@ -0,0 +1,101 @@
+package xyz.thelostsoul.utils;
+
+import com.google.gson.Gson;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * ∏Ò ΩªØJSON
+ */
+public class JsonFormatter {
+
+    private static final String RETURN = "\r\n";
+    private static final String TAB = "    ";
+    private static final String COMMAS = ",";
+    private static final String COLONS = ":";
+    private static final String QUOTE = "\"";
+    private static final String LCURLY = "{";
+    private static final String RCURLY = "}";
+    private static final String LBRACKET = "[";
+    private static final String RBRACKET = "]";
+
+    public static String formatJson(String json) throws Exception {
+        if (StringUtils.isBlank(json)) {
+            return json;
+        }
+        Gson g = new Gson();
+        Map<String, Object> jsonMap = g.fromJson(json, Map.class);
+        return format(jsonMap, 1);
+    }
+
+    public static String format(Map<String, Object> jsonMap, int level) throws Exception {
+        if (MapUtils.isEmpty(jsonMap)) {
+            return "";
+        }
+        StringBuilder prettyJson = new StringBuilder();
+        prettyJson.append(LCURLY).append(RETURN);
+        Set<String> keys = jsonMap.keySet();
+        for (String key : keys) {
+            prettyJson.append(manyString(TAB, level)).append(QUOTE).append(key).append(QUOTE).append(COLONS);
+            Object value = jsonMap.get(key);
+            if (value instanceof Map) {
+                prettyJson.append(format((Map<String, Object>) value, level + 1)).append(COMMAS).append(RETURN);
+            } else if (value instanceof List) {
+                prettyJson.append(LBRACKET).append(RETURN);
+                List tmp = (List) value;
+                for (int i = 0, j = tmp.size(); i < j; i++) {
+                    Object o = tmp.get(i);
+                    if (o instanceof Map) {
+                        prettyJson.append(manyString(TAB, level+1)).append(format((Map<String, Object>) o, level + 2));
+                        if (i != j - 1) {
+                            prettyJson.append(COMMAS);
+                        }
+                        prettyJson.append(RETURN);
+                    } else if (o instanceof String) {
+                        prettyJson.append(QUOTE).append(o).append(QUOTE);
+                        if (i != j - 1) {
+                            prettyJson.append(COMMAS);
+                        }
+                    } else {
+                        prettyJson.append(o);
+                        if (i != j - 1) {
+                            prettyJson.append(COMMAS);
+                        }
+                    }
+                }
+                prettyJson.append(manyString(TAB, level)).append(RBRACKET).append(COMMAS).append(RETURN);
+            } else if (value instanceof String) {
+                prettyJson.append(QUOTE).append(value).append(QUOTE).append(COMMAS).append(RETURN);
+            } else {
+                prettyJson.append(value).append(COMMAS).append(RETURN);
+            }
+        }
+        prettyJson.delete(prettyJson.length()-3, prettyJson.length()-2);
+        prettyJson.append(manyString(TAB, level-1)).append(RCURLY);
+        return prettyJson.toString();
+    }
+
+    public static String unformat(String prettyJson) throws Exception {
+        if (StringUtils.isBlank(prettyJson)) {
+            return prettyJson;
+        }
+
+        return prettyJson.replaceAll("\r\n", "").replaceAll("\n", "").replaceAll("\t", "");
+    }
+
+    private static String manyString(String s, int l) throws Exception {
+        if (s == null) {
+            return s;
+        }
+        StringBuilder r = new StringBuilder();
+        for (int i = 0; i < l; i++) {
+            r.append(s);
+        }
+        return r.toString();
+    }
+}
+
